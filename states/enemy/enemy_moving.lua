7e51c5b22e24ce42a00d35f0b3144e172469a5b3
Tanner Rogalsky <the.t.dawg@gmail.com>
2013-06-13 09:00:58 -0400

    enemy movement optimization

--- states/enemy/enemy_moving.lua
+++ states/enemy/enemy_moving.lua
@@ -9,15 +9,22 @@ function Moving:update(dt)
   local x, y = self:world_center()
   local target_center_x, target_center_y = self.target:world_center()
   self.angle = math.atan2(target_center_y - y, target_center_x - x)
+
+  local old_grid_x, old_grid_y = self.x, self.y
+
   self:move_by_pixel(self.speed * dt * math.cos(self.angle), self.speed * dt * math.sin(self.angle))
 
-  for _, _, sibling in self.parent:each(self.x - 1, self.y - 1, 3, 3) do
-    if sibling then
-      local room = sibling:get_first_content_of_type(TowerRoom)
+  -- we're in a new grid space so check if any of our siblings are TowerRooms
+  if old_grid_x ~= self.x or old_grid_y ~= self.y then
+    for _, _, sibling in self.parent:each(self.x - 1, self.y - 1, 3, 3) do
+      if sibling then
+        local room = sibling:get_first_content_of_type(TowerRoom)
 
-      if room then
-        self:gotoState("MovingToAttackingTransition")
-        break
+        -- if there's a room, transition into a state where we move next to it
+        if room then
+          self:gotoState("MovingToAttackingTransition")
+          break
+        end
       end
     end
   end
