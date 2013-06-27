cec32ec616c2b74391f3e4db60b121a7b22040aa
Tanner Rogalsky <the.t.dawg@gmail.com>
2013-06-27 08:43:17 -0400

    resource rendering

--- resource.lua
+++ resource.lua
@@ -4,6 +4,17 @@ Resource.static.instances = {}
 function Resource:initialize(parent, x, y)
   MapEntity.initialize(self, parent, x, y, 1, 1)
 
+  local small_crystal = game.preloaded_image["crystal_small.png"]
+  local big_crystal = game.preloaded_image["crystal.png"]
+
+  self.canvas = g.newCanvas(self.width * self.parent.tile_width, self.height * self.parent.tile_height)
+  g.setCanvas(self.canvas)
+  g.setColor(COLORS.white:rgb())
+  for i=0,10,5 do
+    g.draw(small_crystal, i, math.random(3))
+  end
+  g.setCanvas(nil)
+
   self.worth = 10
   Resource.instances[self.id] = self
 end
@@ -14,8 +25,8 @@ function Resource:destroy()
 end
 
 function Resource:render()
-  g.setColor(COLORS.magenta:rgb())
-  g.rectangle("fill", self.world_x, self.world_y, self.width * self.parent.tile_width, self.height * self.parent.tile_height)
+  g.setColor(COLORS.white:rgb())
+  g.draw(self.canvas, self.world_x, self.world_y)
 end
 
 Resource.__lt = MapEntity.__lt
